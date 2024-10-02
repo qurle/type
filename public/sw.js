f6cbceaa2eeaba9f2b4ae9de0ffc2c6a8cb57d97
nick <wrl.snk@gmail.com>
2024-10-02 14:18:13 +0300

    updated service worker — now it's online-first, then local

--- public/sw.js
+++ public/sw.js
@@ -51,6 +51,44 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
 	}
 };
 
+const networkFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+
+	// First try to get the resource from the network
+	try {
+		const responseFromNetwork = await fetch(request.clone());
+		// response may be used only once
+		// we need to save clone to put one copy in cache
+		// and serve second one
+		putInCache(request, responseFromNetwork.clone());
+		return responseFromNetwork;
+	} catch (error) {
+		// If that fails, use resource from the cache
+		const responseFromCache = await caches.match(request);
+		if (responseFromCache) {
+			return responseFromCache;
+		}
+
+		const fallbackResponse = await caches.match(fallbackUrl);
+		if (fallbackResponse) {
+			return fallbackResponse;
+		}
+		// when even the fallback response is not available,
+		// there is nothing we can do, but we must always
+		// return a Response object
+		return new Response('Network error happened', {
+			status: 408,
+			headers: { 'Content-Type': 'text/plain' },
+		});
+	}
+
+	// First try to get the resource from the cache
+	const responseFromCache = await caches.match(request);
+	if (responseFromCache) {
+		return responseFromCache;
+	}
+};
+
+
 const enableNavigationPreload = async () => {
 	if (self.registration.navigationPreload) {
 		// Enable navigation preloads!
@@ -73,7 +111,7 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
-		cacheFirst({
+		networkFirst({
 			request: event.request,
 			preloadResponsePromise: event.preloadResponse,
 			fallbackUrl: './fav/favicon.svg',
