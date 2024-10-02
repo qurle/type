cbc741b5a70166f18f3a3829523751c558b3d0d1
nick <wrl.snk@gmail.com>
2024-10-02 21:56:40 +0300

    fixing sw errors

--- public/sw.js
+++ public/sw.js
@@ -4,51 +4,11 @@ const addResourcesToCache = async (resources) => {
 };
 
 const putInCache = async (request, response) => {
+	if (!event.request.url.startsWith('http')) {
+		return
+	}
 	const cache = await caches.open('v1');
 	await cache.put(request, response);
-};
-
-const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-	// First try to get the resource from the cache
-	const responseFromCache = await caches.match(request);
-	if (responseFromCache) {
-		return responseFromCache;
-	}
-
-	// Next try to use the preloaded response, if it's there
-	// NOTE: Chrome throws errors regarding preloadResponse, see:
-	// https://bugs.chromium.org/p/chromium/issues/detail?id=1420515
-	// https://github.com/mdn/dom-examples/issues/145
-	// To avoid those errors, remove or comment out this block of preloadResponse
-	// code along with enableNavigationPreload() and the "activate" listener.
-	const preloadResponse = await preloadResponsePromise;
-	if (preloadResponse) {
-		console.info('using preload response', preloadResponse);
-		putInCache(request, preloadResponse.clone());
-		return preloadResponse;
-	}
-
-	// Next try to get the resource from the network
-	try {
-		const responseFromNetwork = await fetch(request.clone());
-		// response may be used only once
-		// we need to save clone to put one copy in cache
-		// and serve second one
-		putInCache(request, responseFromNetwork.clone());
-		return responseFromNetwork;
-	} catch (error) {
-		const fallbackResponse = await caches.match(fallbackUrl);
-		if (fallbackResponse) {
-			return fallbackResponse;
-		}
-		// when even the fallback response is not available,
-		// there is nothing we can do, but we must always
-		// return a Response object
-		return new Response('Network error happened', {
-			status: 408,
-			headers: { 'Content-Type': 'text/plain' },
-		});
-	}
 };
 
 const networkFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
@@ -118,3 +78,46 @@ self.addEventListener('fetch', (event) => {
 		})
 	);
 });
+
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+	// First try to get the resource from the cache
+	const responseFromCache = await caches.match(request);
+	if (responseFromCache) {
+		return responseFromCache;
+	}
+
+	// Next try to use the preloaded response, if it's there
+	// NOTE: Chrome throws errors regarding preloadResponse, see:
+	// https://bugs.chromium.org/p/chromium/issues/detail?id=1420515
+	// https://github.com/mdn/dom-examples/issues/145
+	// To avoid those errors, remove or comment out this block of preloadResponse
+	// code along with enableNavigationPreload() and the "activate" listener.
+	const preloadResponse = await preloadResponsePromise;
+	if (preloadResponse) {
+		console.debug('using preload response', preloadResponse);
+		putInCache(request, preloadResponse.clone());
+		return preloadResponse;
+	}
+
+	// Next try to get the resource from the network
+	try {
+		const responseFromNetwork = await fetch(request.clone());
+		// response may be used only once
+		// we need to save clone to put one copy in cache
+		// and serve second one
+		putInCache(request, responseFromNetwork.clone());
+		return responseFromNetwork;
+	} catch (error) {
+		const fallbackResponse = await caches.match(fallbackUrl);
+		if (fallbackResponse) {
+			return fallbackResponse;
+		}
+		// when even the fallback response is not available,
+		// there is nothing we can do, but we must always
+		// return a Response object
+		return new Response('Network error happened', {
+			status: 408,
+			headers: { 'Content-Type': 'text/plain' },
+		});
+	}
+}
--- src/utils/del.ts
+++ src/utils/del.ts
@@ -1,6 +1,7 @@
 export function del(noteId: string, opfs: FileSystemDirectoryHandle) {
-	const name = localStorage.getItem(`note-${noteId}`)
-	const confirmation = confirm(`Delete ${name? name: "note"}?`)
+	const name = localStorage.getItem(`name-${noteId}`) || "note"
+	console.debug(`Deleting ${noteId} called ${name}`)
+	const confirmation = confirm(`Delete ${name}?`)
 	if (confirmation){
 	    opfs.removeEntry(noteId)
 		localStorage.removeItem(`note-${noteId}`)
