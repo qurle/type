cb5d856b914a8820a3d20da72b5e800190004c27
nick <wrl.snk@gmail.com>
2024-10-03 17:42:25 +0300

    removed comments

--- src/utils/writeToFile.ts
+++ src/utils/writeToFile.ts
@@ -33,22 +33,6 @@ export async function writeToFile(editor: Editor, editorEl: HTMLElement, opfs: F
 		await writable.close()
 	}
 	catch {
-		// stateEl.innerText = 'can\'t save'
-		// stateEl.animate(
-		// 	[{ opacity: 0 }, { opacity: 1, offset: 0.25 }, { opacity: 1, offset: 0.75 }, { opacity: 0 }],
-		// 	1250,
-		// )
-		// setTimeout(() => {
-		// 	stateEl.innerText = 'on safari'
-		// 	stateEl.animate(
-		// 		[{ opacity: 0 }, { opacity: 1, offset: 0.25 }, { opacity: 1, offset: 0.75 }, { opacity: 0 }],
-		// 		1250,
-		// 	)
-		// }, 1250)
-
-		// opfs.removeEntry(id)
-		// return
-
 		let worker = new Worker('./safari.js')
 
 		worker.postMessage({
