34f228fab8306ab82db454ac2fe738ff40d1968f
nick <wrl.snk@gmail.com>
2025-02-17 23:45:07 +0300

    Just removed semicolons

--- src/scripts/storage/getOpfs.ts
+++ src/scripts/storage/getOpfs.ts
@@ -1,11 +1,11 @@
 export async function getOpfs(directory = 'md', create = true) {
   if (!navigator.storage) {
-    console.error("navigator.storage is not supported in this browser environment. OPFS cannot be used.");
+    console.error("navigator.storage is not supported in this browser environment. OPFS cannot be used.")
     // CRITICAL: Return null or throw an error, since we cannot proceed.
-    return null;
+    return null
   }
 
-  const estimation = await navigator.storage.estimate();
-  console.debug(`Used ${(estimation.usage / estimation.quota * 100).toFixed(2)}% of storage quota`);
-  return (await navigator.storage.getDirectory()).getDirectoryHandle(directory, { create: create });
+  const estimation = await navigator.storage.estimate()
+  console.debug(`Used ${(estimation.usage / estimation.quota * 100).toFixed(2)}% of storage quota`)
+  return (await navigator.storage.getDirectory()).getDirectoryHandle(directory, { create: create })
 }
