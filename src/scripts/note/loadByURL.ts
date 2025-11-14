646de0859dd6784fb8088caf9c8ecf48dab580e0
nick <wrl.snk@gmail.com>
2025-11-14 23:07:20 +0300

    Fixed loading with trailing slash

--- src/scripts/note/loadByURL.ts
+++ src/scripts/note/loadByURL.ts
@@ -14,11 +14,17 @@ export async function loadByURL() {
 		state.editorEl.classList.add('loading')
 		clearCurrentId()
 		lock()
+		// Removing trailing slash
+		const noteId = idEl.dataset.id.replace(/\/$/, '')
 		const { content, clientId } = await fetch(
-			`/api/getPublished?id=${idEl.dataset.id}`,
+			`/api/getPublished?id=${noteId}`,
 		)
 			.then((response) => response.json())
-			.catch(() => showStatus(`couldn't load :(`))
+			.catch((error) => {
+				showStatus(`couldn't load :(`)
+				state.editorEl.classList.remove('loading')
+				insertMD(`Couldn't load note\n\n${JSON.stringify(error)}`)
+			})
 		idEl.remove()
 		insertMD(content)
 		state.editorEl.classList.remove('loading')
