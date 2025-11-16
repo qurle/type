70f147d6ba98610a33cb0cf3508eb8cc0e54d3e9
nick <wrl.snk@gmail.com>
2025-11-16 23:21:05 +0300

    Font moved into the project

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,17 @@
-import { glob } from 'astro/loaders'
-import { defineCollection, getCollection, getEntry, z } from 'astro:content'
+// News not planned but structure may help in future
 
-const news = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/data/news" }),
-	schema: z.object({
-		title: z.string(),
-		date: z.date(),
-	})
-})
+// import { glob } from 'astro/loaders'
+// import { defineCollection, getCollection, getEntry, z } from 'astro:content'
 
-// Export a single `collections` object to register your collection(s)
-export const collections = {
-	news,
-}
+// const news = defineCollection({
+// 	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/data/news" }),
+// 	schema: z.object({
+// 		title: z.string(),
+// 		date: z.date(),
+// 	})
+// })
+
+// // Export a single `collections` object to register your collection(s)
+// export const collections = {
+// 	news,
+// }
