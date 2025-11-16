95b055457029ff2ebec0efc11c523327d7457865
nick <wrl.snk@gmail.com>
2025-11-17 00:23:30 +0300

    uncommented news to fix errors

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,17 @@
 // News not planned but structure may help in future
 
-// import { glob } from 'astro/loaders'
-// import { defineCollection, getCollection, getEntry, z } from 'astro:content'
+import { glob } from 'astro/loaders'
+import { defineCollection, getCollection, getEntry, z } from 'astro:content'
 
-// const news = defineCollection({
-// 	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/data/news" }),
-// 	schema: z.object({
-// 		title: z.string(),
-// 		date: z.date(),
-// 	})
-// })
+const news = defineCollection({
+	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/data/news" }),
+	schema: z.object({
+		title: z.string(),
+		date: z.date(),
+	})
+})
 
-// // Export a single `collections` object to register your collection(s)
-// export const collections = {
-// 	news,
-// }
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+	news,
+}
